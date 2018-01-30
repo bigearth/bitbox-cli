0ae61a3f4e4d1f210ae7edd9a75c9c3916ed1231
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-01-30 08:28:49 -0800

    Initial commit.

---
+++ index.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+let program = require('commander');
+let chalk = require('chalk');
+let mkdirp = require('mkdirp');
+let cpFile = require('cp-file');
+let figlet = require('figlet');
+let clear = require('clear');
+let fs = require('fs');
+let touch = require("touch");
+let emoji = require('node-emoji');
+let repl = require("repl");
+let BITBOXCore = require('./utilities/bitboxcore.js');
+
+// let request = require('superagent');
+// let co = require('co');
+// let prompt = require('co-prompt');
+// let fs = require('fs');
+// let ProgressBar = require('progress');
+
+program
+  .version('0.0.1');
+
+program
+  .command('init')
+  .description('Initialize new and empty BITBOX project')
+  .option('-t, --title <title>', 'Title of new project')
+  .action((options) => {
+    clear();
+    console.log(
+      chalk.blue(
+        figlet.textSync('BITBOX', { horizontalLayout: 'full' })
+      )
+    );
+    let title = options.title;
+    if(!title) {
+      console.log(chalk.bold.red("You didn't provide a title! Using BITBOX instead. #SorryNotSorry"));
+      title = 'BITBOX';
+    }
+
+    console.log(chalk.green(`Creating ${title}/ directory`));
+    console.log(chalk.green(`Creating src/ directory: ./${title}/src`));
+    mkdirp(`./${title}/tests`, (err) => {});
+
+    console.log(chalk.green(`Creating test/ directory: ./${title}/tests`));
+    mkdirp(`./${title}/src`, (err) => {});
+
+    console.log(chalk.green(`Creating bitbox.js configuration file`));
+
+    touch('./bitbox.js');
+    fs.writeFileSync( 'bitbox.js', `module.exports = {
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8332
+    }
+  }
+};`);
+    console.log(chalk.blue('All done.'), emoji.get(':white_check_mark:'));
+    console.log(chalk.blue('Go get em! Remember--with great power comes great responsibility.'), emoji.get(':rocket:'));
+
+    // console.log(chalk.green(`Creatiing test/ directory: ./${title}/tests`));
+    // cpFile('./src/bitbox.js', './bitbox.js').then(() => {
+    //     console.log('File copied');
+    // });
+  }
+);
+
+program
+  .command('console')
+  .description('Run a console with Bitcoin Cash RPC commands available')
+  .action((options) => {
+    var replServer = repl.start({
+      prompt: `${emoji.get(':zap:')} ${" BITBOX"} ${emoji.get(':zap:')} `,
+    });
+    replServer.context.BITBOXCore = BITBOXCore;
+  }
+);
+  // .option('-t, --title <title>', 'Title of new project')
+
+// program
+//   // .arguments('<command>')
+//   // .command('init')
+//   .command('tbox')
+//   .option('-f, --fu <fu>', 'Initialize new and empty BITBOX project')
+//   // .version('0.0.1', '-v, --version')
+//   .action((command) => {
+//     // console.log(command);
+//     mkdirp('./bitbox/tests', (err) => {});
+//     mkdirp('./bitbox/src', (err) => {});
+//     // console.log(chalk.bold.cyan('projectname: ') + program.init);
+//     // console.log(chalk.bold.red('projectname: ') + program.init);
+//   })
+
+program
+  .parse(process.argv);
+
+// console.error(errorMessage);
+// process.exit(1);
---
+++ utilities/bitboxcore.js
@@ -0,0 +1,424 @@
+class BITBOXCore {
+
+  static abandonTransaction() {
+    console.log('abandontransaction called');
+  }
+
+  static addmultisigaddress() {
+    console.log('addmultisigaddress called');
+  }
+
+  static addnode() {
+    console.log('addnode called');
+  }
+
+  static addwitnessaddress() {
+    console.log('addwitnessaddress called');
+  }
+  static backupwallet() {
+    console.log('backupwallet called');
+  }
+
+  static bumpfee() {
+    console.log('bumpfee called');
+  }
+
+  static clearbanned() {
+    console.log('clearbanned called');
+  }
+
+  static createmultisig() {
+    console.log('createmultisig called');
+  }
+
+
+  static createrawtransaction() {
+    console.log('createrawtransaction called');
+  }
+
+  static decoderawtransaction() {
+    console.log('decoderawtransaction called');
+  }
+
+  static decodescript() {
+    console.log('decodescript called');
+  }
+
+  static disconnectnode() {
+    console.log('disconnectnode called');
+  }
+
+  static dumpprivkey() {
+    console.log('dumpprivkey called');
+  }
+
+  static dumpwallet() {
+    console.log('dumpwallet called');
+  }
+
+  static encryptwallet() {
+    console.log('encryptwallet called');
+  }
+
+  static estimatefee() {
+    console.log('estimatefee called');
+  }
+
+  static estimatepriority() {
+    console.log('estimatepriority called');
+  }
+
+  static fundrawtransaction() {
+    console.log('fundrawtransaction called');
+  }
+
+  static generate() {
+    console.log('generate called');
+  }
+
+  static generatetoaddress() {
+    console.log('generatetoaddress called');
+  }
+
+  static getaccountaddress() {
+    console.log('getaccountaddress called');
+  }
+
+  static getaccount() {
+    console.log('getaccount called');
+  }
+
+  static getaddednodeinfo() {
+    console.log('getaddednodeinfo called');
+  }
+
+  static getaddressesbyaccount() {
+    console.log('getaddressesbyaccount called');
+  }
+
+  static getbalance() {
+    console.log('getbalance called');
+  }
+
+  static getbestblockhash() {
+    console.log('getbestblockhash called');
+  }
+
+  static getblock() {
+    console.log('getblock called');
+  }
+
+  static getblockchaininfo() {
+    console.log('getblockchaininfo called');
+  }
+
+  static getblockcount() {
+    console.log('getblockcount called');
+  }
+
+  static getblockhash() {
+    console.log('getblockhash called');
+  }
+
+  static getblockheader() {
+    console.log('getblockheader called');
+  }
+
+  static getblocktemplate() {
+    console.log('getblocktemplate called');
+  }
+
+  static getchaintips() {
+    console.log('getchaintips called');
+  }
+
+  static getconnectioncount() {
+    console.log('getconnectioncount called');
+  }
+
+  static getdifficulty() {
+    console.log('getdifficulty called');
+  }
+
+  static getgenerate() {
+    console.log('getgenerate called');
+  }
+
+  static gethashespersec() {
+    console.log('gethashespersec called');
+  }
+
+  static getinfo() {
+    console.log('getinfo called');
+  }
+
+  static getmemoryinfo() {
+    console.log('getmemoryinfo called');
+  }
+
+  static getmempoolancestors() {
+    console.log('getmempoolancestors called');
+  }
+
+  static getmempooldescendants() {
+    console.log('getmempooldescendants called');
+  }
+
+  static getmempoolentry() {
+    console.log('getmempoolentry called');
+  }
+
+  static getmempoolinfo() {
+    console.log('getmempoolinfo called');
+  }
+
+  static getmininginfo() {
+    console.log('getmininginfo called');
+  }
+
+  static getnettotals() {
+    console.log('getnettotals called');
+  }
+
+  static getnetworkhashps() {
+    console.log('getnetworkhashps called');
+  }
+
+  static getnetworkinfo() {
+    console.log('getnetworkinfo called');
+  }
+
+  static getnewaddress() {
+    console.log('getnewaddress called');
+  }
+
+  static getpeerinfo() {
+    console.log('getpeerinfo called');
+  }
+
+  static getrawchangeaddress() {
+    console.log('getrawchangeaddress called');
+  }
+
+  static getrawmempool() {
+    console.log('getrawmempool called');
+  }
+
+  static getrawtransaction() {
+    console.log('getrawtransaction called');
+  }
+
+  static getreceivedbyaccount() {
+    console.log('getreceivedbyaccount called');
+  }
+
+  static getreceivedbyaddress() {
+    console.log('getreceivedbyaddress called');
+  }
+
+  static gettransaction() {
+    console.log('gettransaction called');
+  }
+
+  static gettxout() {
+    console.log('gettxout called');
+  }
+
+  static gettxoutproof() {
+    console.log('gettxoutproof called');
+  }
+
+  static gettxoutsetinfo() {
+    console.log('gettxoutsetinfo called');
+  }
+
+  static getunconfirmedbalance() {
+    console.log('getunconfirmedbalance called');
+  }
+
+  static getwalletinfo() {
+    console.log('getwalletinfo called');
+  }
+
+  static getwork() {
+    console.log('getwork called');
+  }
+
+  static help() {
+    console.log('help called');
+  }
+
+  static importaddress() {
+    console.log('importaddress called');
+  }
+
+  static importmulti() {
+    console.log('importmulti called');
+  }
+
+  static importprivkey() {
+    console.log('importprivkey called');
+  }
+
+  static importprunedfunds() {
+    console.log('importprunedfunds called');
+  }
+
+  static importwallet() {
+    console.log('importwallet called');
+  }
+
+  static keypoolrefill() {
+    console.log('keypoolrefill called');
+  }
+
+  static listaccounts() {
+    console.log('listaccounts called');
+  }
+
+  static listaddressgroupings() {
+    console.log('listaddressgroupings called');
+  }
+
+  static listbanned() {
+    console.log('listbanned called');
+  }
+
+  static listlockunspent() {
+    console.log('listlockunspent called');
+  }
+
+  static listreceivedbyaccount() {
+    console.log('listreceivedbyaccount called');
+  }
+
+  static listreceivedbyaddress() {
+    console.log('listreceivedbyaddress called');
+  }
+
+  static listsinceblock() {
+    console.log('listsinceblock called');
+  }
+
+  static listtransactions() {
+    console.log('listtransactions called');
+  }
+
+  static listunspent() {
+    console.log('listunspent called');
+  }
+
+  static lockunspent() {
+    console.log('lockunspent called');
+  }
+
+  static move() {
+    console.log('move called');
+  }
+
+  static pingRpc() {
+    console.log('ping-rpc called');
+  }
+
+  static preciousblock() {
+    console.log('preciousblock called');
+  }
+
+  static prioritisetransaction() {
+    console.log('prioritisetransaction called');
+  }
+
+  static pruneblockchain() {
+    console.log('pruneblockchain called');
+  }
+
+  static removeprunedfunds() {
+    console.log('removeprunedfunds called');
+  }
+
+  static sendfrom() {
+    console.log('sendfrom called');
+  }
+
+  static sendmany() {
+    console.log('sendmany called');
+  }
+
+  static sendrawtransaction() {
+    console.log('sendrawtransaction called');
+  }
+
+  static sendtoaddress() {
+    console.log('sendtoaddress called');
+  }
+
+  static setaccount() {
+    console.log('setaccount called');
+  }
+
+  static setban() {
+    console.log('setban called');
+  }
+
+  static setgenerate() {
+    console.log('setgenerate called');
+  }
+
+  static setnetworkactive() {
+    console.log('setnetworkactive called');
+  }
+
+  static settxfee() {
+    console.log('settxfee called');
+  }
+
+  static signmessage() {
+    console.log('signmessage called');
+  }
+
+  static signmessagewithprivkey() {
+    console.log('signmessagewithprivkey called');
+  }
+
+  static signrawtransaction() {
+    console.log('signrawtransaction called');
+  }
+
+  static stop() {
+    console.log('stop called');
+  }
+
+  static submitblock() {
+    console.log('submitblock called');
+  }
+
+  static validateaddress() {
+    console.log('validateaddress called');
+  }
+
+  static verifychain() {
+    console.log('verifychain called');
+  }
+
+  static verifymessage() {
+    console.log('verifymessage called');
+  }
+
+  static verifytxoutproof() {
+    console.log('verifytxoutproof called');
+  }
+
+  static walletlock() {
+    console.log('walletlock called');
+  }
+
+  static walletpassphrase() {
+    console.log('walletpassphrase called');
+  }
+
+  static walletpassphrasechange() {
+    console.log('walletpassphrasechange called');
+  }
+}
+
+module.exports = BITBOXCore;
