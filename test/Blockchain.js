2302b63f9a6b0675a62ce4a87e75a95530f78851
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-04-27 10:50:05 -0500

    test.

--- test/Blockchain.js
+++ test/Blockchain.js
@@ -24,7 +24,7 @@ describe('#axios', () => {
     server.restore();
     sandbox.restore();
   });
-  it('should display a blankslate', (done) => {
+  it('should display', (done) => {
     BITBOX.Blockchain.getBestBlockHash()
       .then((result) => {
         console.log(result);
@@ -33,7 +33,19 @@ describe('#axios', () => {
     setTimeout(() => server.respond([
       200,
       { 'Content-Type': 'application/json' },
-      JSON.stringify({data: '241decef88889efac8e6ce428a8ac696fdde5972eceed97e1fb58d6106af31d5'})
+      JSON.stringify({
+        "version": 170000,
+        "protocolversion": 70015,
+        "blocks": 527634,
+        "timeoffset": 0,
+        "connections": 14,
+        "proxy": "",
+        "difficulty": 541745568133.4969,
+        "testnet": false,
+        "paytxfee": 0,
+        "relayfee": 0.00001,
+        "errors": ""
+      })
     ]), 0);
   });
 });
