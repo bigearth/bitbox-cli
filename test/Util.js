ab3a59b30124fb112865266283b261b9a3d4366f
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-03 08:01:21 -0700

    tests

--- test/Util.js
+++ test/Util.js
@@ -7,57 +7,6 @@ let BITBOX = new BITBOXCli();
 let sinon = require('sinon');
 
 describe('#Util', () => {
-  describe('#createMultisig', () => {
-    let sandbox;
-    beforeEach(() => sandbox = sinon.sandbox.create());
-    afterEach(() => sandbox.restore());
-
-    it('should create multisig', (done) => {
-      let data = {
-        result: {
-          address: 'bitcoincash:pzgdursgu5k3qrx6k320crkxv30tq28n2q93yddha8',
-          redeemScript: '51210216f220122e31c5228eff825020fa3b7ae2770385f0770a6a440f4453225e77ca2103070c2f8b9cfc347470849c46d2efc89747c14a4f62e55143a28f62914e9cd50352ae'
-        }
-      };
-
-      const resolved = new Promise((r) => r({ data: data }));
-      sandbox.stub(axios, 'post').returns(resolved);
-
-      BITBOX.Util.createMultisig(1, ["0216f220122e31c5228eff825020fa3b7ae2770385f0770a6a440f4453225e77ca", "03070c2f8b9cfc347470849c46d2efc89747c14a4f62e55143a28f62914e9cd503"])
-      .then((result) => {
-        assert.deepEqual(
-          data.result,
-          result
-        );
-      })
-      .then(done, done);
-    });
-  });
-
-  describe('#signMessageWithPrivKey', () => {
-    let sandbox;
-    beforeEach(() => sandbox = sinon.sandbox.create());
-    afterEach(() => sandbox.restore());
-
-    it('should sign message w/ privkey', (done) => {
-      let data = {
-        result: 'H8GjNeEdrj/p6waepyYXwlZPdBH4l69xQjyMBOR0m7lCYb2lNWDicp/M6qlZQlXVoJsz0m8/h87hpvI9F8oBVBY='
-      };
-
-      const resolved = new Promise((r) => r({ data: data }));
-      sandbox.stub(axios, 'post').returns(resolved);
-
-      BITBOX.Util.signMessageWithPrivKey('KzkYKDZw9PveUu3mn4nR5LKYqmY1A6yHFdY8YqQvd7Z2BT3Ssu4Y', 'BITBOX')
-      .then((result) => {
-        assert.deepEqual(
-          data.result,
-          result
-        );
-      })
-      .then(done, done);
-    });
-  });
-
   describe('#validateAddress', () => {
     let sandbox;
     beforeEach(() => sandbox = sinon.sandbox.create());
@@ -79,30 +28,6 @@ describe('#Util', () => {
       sandbox.stub(axios, 'get').returns(resolved);
 
       BITBOX.Util.validateAddress('bitcoincash:qpz7qtkuyhrsz4qmnnrvf8gz9zd0u9v7eqsewyk4w5')
-      .then((result) => {
-        assert.deepEqual(
-          data.result,
-          result
-        );
-      })
-      .then(done, done);
-    });
-  });
-
-  describe('#verifyMessage', () => {
-    let sandbox;
-    beforeEach(() => sandbox = sinon.sandbox.create());
-    afterEach(() => sandbox.restore());
-
-    it('should verify message', (done) => {
-      let data = {
-        result: true
-      };
-
-      const resolved = new Promise((r) => r({ data: data }));
-      sandbox.stub(axios, 'get').returns(resolved);
-
-      BITBOX.Util.verifyMessage('bitcoincash:qqy7dlefcrknfsl0d2jfnf7pe0r2tt6vqy2pedtdfu', 'H8GjNeEdrj/p6waepyYXwlZPdBH4l69xQjyMBOR0m7lCYb2lNWDicp/M6qlZQlXVoJsz0m8/h87hpvI9F8oBVBY=', 'BITBOX')
       .then((result) => {
         assert.deepEqual(
           data.result,
