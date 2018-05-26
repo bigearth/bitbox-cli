187ef53616a62e1eceb16445f2503743257f910d
Gabriel Cardona <gabriel.cardona@clone.earth>
2018-05-26 07:42:43 -0700

    Raw transactions.

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let cmd = require('node-cmd');
 // let ProgressBar = require('progress');
 
 program
-  .version('0.8.0');
+  .version('0.8.1');
 
 program
   .command('new <name>')
--- test/RawTransactions.js
+++ test/RawTransactions.js
@@ -3,7 +3,14 @@ let chai = require('chai');
 let assert = require('assert');
 let axios = require('axios');
 let BITBOXCli = require('./../lib/bitboxcli').default;
-let BITBOX = new BITBOXCli();
+let BITBOX = new BITBOXCli({
+  protocol: 'http',
+  host: "138.68.54.100",
+  port: "8332",
+  username: "bitcoin",
+  password: "xhFjluMJMyOXcYvF"
+});
+
 let sinon = require('sinon');
 
 describe('#RawTransactions', () => {
@@ -14,15 +21,13 @@ describe('#RawTransactions', () => {
 
     it('should decode raw transaction', (done) => {
       let data = {
-        result: {
-          txid: '4ebd325a4b394cff8c57e8317ccf5a8d0e2bdf1b8526f8aad6c8e43d8240621a',
-          hash: '4ebd325a4b394cff8c57e8317ccf5a8d0e2bdf1b8526f8aad6c8e43d8240621a',
-          size: 10,
-          version: 2,
-          locktime: 0,
-          vin: [],
-          vout: []
-        }
+        "txid": "4ebd325a4b394cff8c57e8317ccf5a8d0e2bdf1b8526f8aad6c8e43d8240621a",
+        "hash": "4ebd325a4b394cff8c57e8317ccf5a8d0e2bdf1b8526f8aad6c8e43d8240621a",
+        "size": 10,
+        "version": 2,
+        "locktime": 0,
+        "vin": [],
+        "vout": []
       };
 
       const resolved = new Promise((r) => r({ data: data }));
@@ -31,8 +36,8 @@ describe('#RawTransactions', () => {
       BITBOX.RawTransactions.decodeRawTransaction('02000000000000000000')
       .then((result) => {
         assert.deepEqual(
-          data.result,
-          result
+          data,
+          JSON.parse(result)
         );
       })
       .then(done, done);
@@ -46,11 +51,9 @@ describe('#RawTransactions', () => {
 
     it('should decode script', (done) => {
       let data = {
-        result: {
-          asm: 'OP_RETURN 5361746f736869204e616b616d6f746f',
-          type: 'nulldata',
-          p2sh: 'bitcoincash:prswx5965nfumux9qng5kj8hw603vcne7q08t8c6jp'
-        }
+        asm: 'OP_RETURN 5361746f736869204e616b616d6f746f',
+        type: 'nulldata',
+        p2sh: 'bitcoincash:prswx5965nfumux9qng5kj8hw603vcne7q08t8c6jp'
       };
 
       const resolved = new Promise((r) => r({ data: data }));
@@ -59,7 +62,7 @@ describe('#RawTransactions', () => {
       BITBOX.RawTransactions.decodeScript('6a105361746f736869204e616b616d6f746f')
       .then((result) => {
         assert.deepEqual(
-          data.result,
+          data,
           result
         );
       })
@@ -95,17 +98,15 @@ describe('#RawTransactions', () => {
     afterEach(() => sandbox.restore());
 
     it('should send raw transaction', (done) => {
-      let data = {
-        result: '808d617eccaad4f1397fe07a06ec5ed15a0821cf22a3e0931c0c92aef9e572b6'
-      };
+      let data = 'Error: transaction already in block chain';
 
       const resolved = new Promise((r) => r({ data: data }));
       sandbox.stub(axios, 'post').returns(resolved);
 
       BITBOX.RawTransactions.sendRawTransaction('020000000160d663961c63c7f0a07f22ec07b8f55b3935bfdbed8b1d8454916e8932fbf109010000006b4830450221008479fab4cfdcb111833d250a43f98ac26d43272b7a29cb1b9a0491eae5c44b3502203448b17253632395c29a7d62058bbfe93efb20fc8636ba6837002d464195aec04121029123258f7cdcd45b864066bcaa9b71f24d5ed1fa1dd36eaf107d8432b5014658ffffffff016d180000000000001976a91479d3297d1823149f4ec61df31d19f2fad5390c0288ac00000000')
       .then((result) => {
-        assert.deepEqual(
-          data.result,
+        assert.equal(
+          data,
           result
         );
       })
