4797128854063fd45155b89fa65d23e9610e17c4
amitrana007 <40827367+amitrana007@users.noreply.github.com>
2018-07-13 00:11:34 +0530

    uploading TransactionBuilder.d.ts

--- @types/TransactionBuilder.d.ts
+++ @types/TransactionBuilder.d.ts
@@ -1,46 +1,26 @@
 import Bitcoin from 'bitcoincashjs-lib';
 import bchaddr from 'bchaddrjs';
-import coininfo from'coininfo';
+import coininfo from 'coininfo';
 import bip66 from 'bip66';
 import bip68 from 'bip68';
 
 export default class TransactionBuilder {
   
   bitcoincash: coininfo.bitcoincash.main;
-  bitcoincashBitcoinJSLib: bintcoincash.toBitcoinJS;
+  bitcoincashBitcoinJSLib: Bitcoin.toBitcoinJS;
   transaction: Bitcoin.Transaction;
-  DEFAULT_SEQUENCE = 0xfffffff;
+  DEFAULT_SEQUENCE:string;
   hashTypes:Enumerator;
-  bip66 = new bip66();
-  bip68 = new bip68();
+  bip66 : bip66;
+  bip68 : bip68;
 
   constructor(network:string);
   
-  addInput(txHash, vout, sequence=string, prevOutScript) {
-    this.transaction.addInput(
-      txHash,
-      vout,
-      sequence,
-      prevOutScript
-    );
-  }
+  addInput(txHash:string, vout:string, sequence:string, prevOutScript:string):void;
 
-  addOutput(scriptPubKey, amount) {
-    try {
-      this.transaction.addOutput(bchaddr.toLegacyAddress(scriptPubKey), amount);
-    }
-    catch(error) {
-      this.transaction.addOutput(scriptPubKey, amount);
-    }
-  }
+  addOutput(scriptPubKey:string, amount:number):void;
 
-  sign(vin, keyPair, redeemScript, hashType = this.hashTypes.SIGHASH_ALL, value) {
-    let witnessScript;
+  sign(vin:string, keyPair:string, redeemScript:string, hashType:Enumerator, value:any):void;
 
-    this.transaction.sign(vin, keyPair, redeemScript, hashType, value, witnessScript);
-  }
-
-  build() {
-    return this.transaction.build();
-  }
+  build():Bitcoin.Transaction;
 }
