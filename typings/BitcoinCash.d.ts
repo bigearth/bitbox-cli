9ecbca47b50c75690fe11597b68c3730320ab676
haryu703 <haryu703@gmail.com>
2018-10-07 18:08:59 +0900

    BitcoinCash.js supports regtest
    only bip21 encode

--- typings/BitcoinCash.d.ts
+++ typings/BitcoinCash.d.ts
@@ -7,7 +7,7 @@ export declare interface BitcoinCash {
 	verifyMessage(address : string, signature: string, message: string): Boolean;
 	encodeBase58Check(hex: string): string;
 	decodeBase58Check(address: string): string;
-	encodeBIP21(address: string, options: EncodeBIP21Options): string;
+	encodeBIP21(address: string, options: EncodeBIP21Options, regtest?: boolean): string;
 	decodeBIP21(url: string): BIP21Object;
 	getByteCount(inputs: ByteCountInput|object, outputs: ByteCountOutput): number;
 	encryptBIP38(privKeyWIF: string, passphrase: string): string; 
