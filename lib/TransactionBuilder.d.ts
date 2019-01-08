a20225afce76dfbcc018e72d148182fac59cdc84
Gabriel Cardona <gabriel@bitcoin.com>
2019-01-08 10:20:46 -0800

    Add setLockTime typing and bump minor version.

--- lib/TransactionBuilder.d.ts
+++ lib/TransactionBuilder.d.ts
@@ -1,33 +1,44 @@
-import {Buffer} from 'buffer';
+import { Buffer } from "buffer"
 
-import { Transaction } from './Transaction';
-import { ECPair } from './ECPair';
+import { Transaction } from "./Transaction"
+import { ECPair } from "./ECPair"
 
-declare type ECSignature = any;
+declare type ECSignature = any
 
 export declare interface TransactionBuilder {
   // bitcoincash: coininfo.bitcoincash.main;
   // bitcoincashBitcoinJSLib: Bitcoin.toBitcoinJS;
-  transaction: any;
-  DEFAULT_SEQUENCE: number;
-  hashTypes: HashTypes;
-  bip66 : any;
-  bip68 : any;
+  transaction: any
+  DEFAULT_SEQUENCE: number
+  hashTypes: HashTypes
+  bip66: any
+  bip68: any
 
-  new(network?: string): TransactionBuilder;
+  new (network?: string): TransactionBuilder
 
-  addInput(txHash: string|Buffer, vout: number, sequence?: number, prevOutScript?: string): void;
-  addInputScript(vout: number, script: Buffer): void;
-  addOutput(scriptPubKey: string|Buffer, amount: number): void;
-  setLockTime(locktime: number): void;
-  sign(vin: number, keyPair: ECPair, redeemScript: Buffer | undefined, hashType: number, value: number): ECSignature;
-  build(): any;
+  addInput(
+    txHash: string | Buffer,
+    vout: number,
+    sequence?: number,
+    prevOutScript?: string
+  ): void
+  addInputScript(vout: number, script: Buffer): void
+  addOutput(scriptPubKey: string | Buffer, amount: number): void
+  setLockTime(locktime: number): void
+  sign(
+    vin: number,
+    keyPair: ECPair,
+    redeemScript: Buffer | undefined,
+    hashType: number,
+    value: number
+  ): ECSignature
+  build(): any
 }
 
 declare interface HashTypes {
-  SIGHASH_ALL: number;
-  SIGHASH_NONE: number;
-  SIGHASH_SINGLE: number;
-  SIGHASH_ANYONECANPAY: number;
-  SIGHASH_BITCOINCASH_BIP143: number;
+  SIGHASH_ALL: number
+  SIGHASH_NONE: number
+  SIGHASH_SINGLE: number
+  SIGHASH_ANYONECANPAY: number
+  SIGHASH_BITCOINCASH_BIP143: number
 }
