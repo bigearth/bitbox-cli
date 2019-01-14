0b428f3d23f65490ee9489ea392985157e3a0295
Chris Troutner <chris.troutner@gmail.com>
2019-01-14 14:52:01 -0800

    Adding detailsByHash integration test

--- test/integration/block.js
+++ test/integration/block.js
@@ -7,7 +7,6 @@ const chai = require("chai")
 const assert = chai.assert
 const BITBOXSDK = require("../../lib/bitbox-sdk").default
 const BITBOX = new BITBOXSDK()
-const axios = require("axios")
 
 // Inspect utility used for debugging.
 const util = require("util")
@@ -18,11 +17,41 @@ util.inspect.defaultOptions = {
 }
 
 describe(`#block`, () => {
-  describe(`#details`, () => {
-    it(`should GET block details`, async () => {
+  describe(`#detailsByHeight`, () => {
+    it(`should GET block details for a given Height`, async () => {
       const block = 500000
 
       const result = await BITBOX.Block.detailsByHeight(block)
+      //console.log(`result: ${util.inspect(result)}`)
+
+      assert.hasAllKeys(result, [
+        "hash",
+        "size",
+        "height",
+        "version",
+        "merkleroot",
+        "tx",
+        "time",
+        "nonce",
+        "bits",
+        "difficulty",
+        "chainwork",
+        "confirmations",
+        "previousblockhash",
+        "nextblockhash",
+        "reward",
+        "isMainChain",
+        "poolInfo"
+      ])
+    })
+  })
+
+  describe("#detailsByHash", () => {
+    it(`should GET block details for a given hash`, async () => {
+      const hash =
+        "000000000000000005e14d3f9fdfb70745308706615cfa9edca4f4558332b201"
+
+      const result = await BITBOX.Block.detailsByHash(hash)
       //console.log(`result: ${util.inspect(result)}`)
 
       assert.hasAllKeys(result, [
