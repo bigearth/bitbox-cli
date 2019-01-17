85001f3f7c68be7759aeb8a52ba28ed74f9a2af1
Chris Troutner <chris.troutner@gmail.com>
2019-01-17 13:54:51 -0800

    Re-checked integration tests

--- src/bitbox-sdk.js
+++ src/bitbox-sdk.js
@@ -24,8 +24,8 @@ class BITBOXSDK {
   constructor(config) {
     if (config && config.restURL && config.restURL !== "")
       this.restURL = config.restURL
-    //else this.restURL = "https://rest.btctest.net/v2/"
-    else this.restURL = "http://localhost:3000/v2/"
+    else this.restURL = "https://rest.btctest.net/v2/"
+    //else this.restURL = "http://localhost:3000/v2/"
 
     this.Address = new Address(this.restURL)
     this.BitcoinCash = new BitcoinCash(this.Address)
