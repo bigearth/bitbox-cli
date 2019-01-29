9ae7e34d20e6a4597aa80a1765d18c4fb31e9dac
Chris Troutner <chris.troutner@gmail.com>
2019-01-29 11:21:12 -0800

    Refactored util.js

--- src/Util.js
+++ src/Util.js
@@ -6,10 +6,28 @@ class Util {
 
   async validateAddress(address) {
     try {
-      const response = await axios.get(
-        `${this.restURL}util/validateAddress/${address}`
-      )
-      return response.data
+      // Single block
+      if (typeof address === "string") {
+        const response = await axios.get(
+          `${this.restURL}util/validateAddress/${address}`
+        )
+        return response.data
+
+        // Array of blocks.
+      } else if (Array.isArray(id)) {
+        const options = {
+          method: "POST",
+          url: `${this.restURL}util/validateAddress`,
+          data: {
+            addresses: address
+          }
+        }
+        const response = await axios(options)
+
+        return response.data
+      }
+
+      throw new Error(`Input must be a string or array of strings.`)
     } catch (error) {
       if (error.response && error.response.data) throw error.response.data
       else throw error
