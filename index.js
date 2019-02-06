5c4b6ae3d0708ddc69e1d6944642a1f2988e23b1
Gabriel Cardona <gabriel@bitcoin.com>
2019-02-06 09:43:57 +0900

    Rebuild.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const repl = require("repl")
 const BITBOXSDK = require("./lib/bitbox-sdk").default
 const clone = require("git-clone")
 
-program.version("3.0.9 ", "-v, --version")
+program.version("3.0.10 ", "-v, --version")
 
 program
   .command("new <name>")
