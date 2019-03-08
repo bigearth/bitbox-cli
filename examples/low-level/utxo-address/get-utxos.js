d269e23b23a5084c274f622c91e306f88241b0bf
Chris Troutner <chris.troutner@gmail.com>
2019-03-08 09:42:28 -0800

    Updating get-utxo example to work with wallet

--- examples/low-level/utxo-address/get-utxos.js
+++ examples/low-level/utxo-address/get-utxos.js
@@ -3,21 +3,35 @@
   with the create-wallet example.
 */
 
-"use strict"
+// Set NETWORK to either testnet or mainnet
+const NETWORK = `testnet`
 
 // Instantiate BITBOX.
 const bitboxLib = "../../../lib/BITBOX"
 const BITBOXSDK = require(bitboxLib)
-const BITBOX = new BITBOXSDK({ restURL: "https://trest.bitcoin.com/v2/" })
 
-const ADDR = `bchtest:qr45kxqda7yw8atztvkc4ckqnrlhmp0kvsep4p345q`
+// Instantiate SLP based on the network.
+let BITBOX
+if (NETWORK === `mainnet`)
+  BITBOX = new BITBOXSDK({ restURL: `https://rest.bitcoin.com/v2/` })
+else BITBOX = new BITBOXSDK({ restURL: `https://trest.bitcoin.com/v2/` })
+
+// Open the wallet generated with create-wallet.
+try {
+  var walletInfo = require(`../../applications/wallet/create-wallet/wallet.json`)
+} catch (err) {
+  console.log(
+    `Could not open wallet.json. Generate a wallet with create-wallet first.`
+  )
+  process.exit(0)
+}
 
 async function getUtxos() {
   try {
     // first get BCH balance
-    const utxos = await BITBOX.Address.utxo(ADDR)
+    const utxos = await BITBOX.Address.utxo(walletInfo.cashAddress)
 
-    console.log(`UTXO information for address ${ADDR}:`)
+    console.log(`UTXO information for address ${walletInfo.cashAddress}:`)
     console.log(utxos)
   } catch (err) {
     console.error(`Error in getUtxos: `, err)
