ca5f86719bbdc085bcdf39fa254177b46513507d
josephroyking <joey@bitcoin.com>
2019-03-19 05:18:32 -0700

    Adding example parsing function

--- examples/low-level/OP_RETURN/op_return.js
+++ examples/low-level/OP_RETURN/op_return.js
@@ -1,6 +1,5 @@
 /*
-  Check the balance of the root address of an HD node wallet generated
-  with the create-wallet example.
+  Check the outputs of a given transaction for messages in OP_Return
 */
 
 // Instantiate BITBOX.
@@ -13,26 +12,53 @@ const BITBOX = new BITBOXSDK()
 const txid = `5b81b332c8fa5a2b2e77bb928bd18072af4485f02a7325d346f1f28cf3d4a6bb`
 
 function parseOP_RETURN(txid) {
-  console.log(`Parsing OP_Return value of transaction ${txid}`)
+  console.log(`Parsing transaction ${txid} for messages in OP_RETURN...`)
+  console.log(``)
 
-  // Get transaction details
+  // Get transaction details from txid
   BITBOX.Transaction.details(txid).then(
     tx => {
-      console.log(tx)
+      // You may wish to log this tx info to the console to inspect and plan your parsing function
+      // console.log(tx)
+      
       // Begin parsing transaction
 
-      // Construct object to hold info for one transaction
-      let parsedTx = {}
+      // Initialize an array to store any OP_Return messages
+      let messages = []
 
-      // Initialize the amount
-      parsedTx.amount = 0
+      // Iterate over outputs looking for OP_Return outputs
+    
+      for (let i=0; i < tx.vout.length; i++) {        
+        
+        // If this is an OP_Return output        
+        if (typeof tx.vout[i].scriptPubKey.addresses === 'undefined') {
+          
+          let message = ''
 
-      // Initialize the OP_Return message
-      let message = ''
+          // Decode the OP_Return message
+          message = tx.vout[i].scriptPubKey.asm
 
-      // Get txid
-      parsedTx.txid = tx.txid
+          let fromAsm = BITBOX.Script.fromASM(message)
+          let decoded = BITBOX.Script.decode(fromAsm)
+          message = decoded[1].toString('ascii')
 
+          // Add this decoded OP_Return message to an array, in case multiple outputs have OP_Return messages
+          messages.push(message)
+        }
+      }
+      
+      if (messages.length === 1) {
+        console.log(`Message found!`)
+        console.log(``)
+        console.log(`Message: ${messages[0]}`)
+      }
+      else {
+        console.log(`${messages.length} messages found!`)
+        console.log(``)
+        for (let j=0; j < messages.length; j++) {
+          console.log(`Message ${j+1} of ${messages.length+1}: ${messages[j]}`)
+        }
+      }
     },
     err => {
       console.log('Error in BITBOX.Transaction.details(${txid}):')
@@ -42,116 +68,3 @@ function parseOP_RETURN(txid) {
 }
 
 parseOP_RETURN(txid)
-
-
-    /*
-
-    // Get sending address
-    // Note this would be txs[i].vin[0].addr from address bulk tx API endpoint
-    parsedTx.sendingAddress = tx.vin[0].legacyAddress
-
-    // From tx
-    parsedTx.fromTxid = tx.vin[0].txid
-
-    // Iterate over outputs
-    if (tx.vout[0]) {      
-      for (let j=0; j< tx.vout.length; j++) {        
-        // If this is an OP_Return output        
-        if (!tx.vout[j].scriptPubKey.addresses[0]) {
-          
-          // Decode the OP_Return message
-          message = tx.vout[j].scriptPubKey.asm            
-          let fromAsm = BITBOX.Script.fromASM(message)
-          let decoded = BITBOX.Script.decode(fromAsm)
-
-          message = decoded[1].toString('ascii')
-
-          // Keep iterating through outputs
-          continue
-        }
-        // If this transaction was asked to Satoshi, i.e. sent from a user to the game
-        else if (tx.vout[j].scriptPubKey.addresses[0] === this.props.address) {
-          // Calculate parsedTx.amount for a question
-          parsedTx.amount += parseInt(tx.vout[j].value*1e8)
-        }
-        else {
-          continue
-        }        
-      }
-    }
-
-    // If this tx was sent by Satoshi, i.e. sent from the game to a user
-    if (parsedTx.sendingAddress === this.props.address) {
-
-      // Amount is amount sent
-      parsedTx.amount = tx.valueIn*1e8
-      
-      // parsed OP_Return message is what Satoshi Said
-      parsedTx.satoshiSaid = message
-        
-      // This tx is an answer from Satoshi
-      // parsedTX: {txid, amount, sendingAddress, fromTxid, satoshiSaid}
-
-      // BEGIN
-      // Find the asking question by checking the state for questions with no answer
-      // Get an array of the last 5 entries into the answers state
-      let potentialQuestions = this.state.answers.slice(0,5)
-      console.log(`potentialQuestions:`)
-      console.log(potentialQuestions)
-
-      // Iterate over array for a question and check if it belongs to this answer      
-      
-      for (let a=0; a < 5; a++) {
-        // If this entry is a question and belongs to this answer
-        console.log(potentialQuestions[a].userAsked)
-        console.log(potentialQuestions[a].txid)
-        console.log(parsedTx.fromTxid)
-        if (potentialQuestions[a].userAsked && potentialQuestions[a].txid === parsedTx.fromTxid) {
-          // Add the userAsked field to parsedTX          
-          parsedTx.userAsked = potentialQuestions[a].userAsked
-
-          console.log(`parsedTx, (answer, ${parsedTx.txid}):`)
-          console.log(parsedTx)
-          
-          //TODO make this more robust
-          // Either have a bail out function to reset display table by refreshing for all tx if this doesn't work
-          // Or be more specific than handling only one tx back
-          return this.setState({      
-            latestTxId: parsedTx.txid,
-            answers: [parsedTx, ...this.state.answers.slice(1)]
-          })
-        }        
-      }
-      // If you didn't find a question, handle it here
-      console.log(`No question found for answer ${parsedTx.txid}`)
-      console.log(parsedTx)
-      
-      
-         
-    }
-    else {
-      // If the asking transaction did not include an OP_Return message
-      if (message === '') {
-        // Display a default placeholder
-        parsedTx.userAsked = 'Known Unto You'        
-      }
-      // If the asking transaction did include an OP_Return message
-      else {
-        // Assign the OP_Return value parsed earlier in the loop as a question
-        parsedTx.userAsked = message        
-        console.log(`Someone asked, ${parsedTx.userAsked}`)
-      }
-
-      console.log(`parsedTx (question, ${parsedTx.txid}):`)
-      console.log(parsedTx)
-
-      // Add question to state to render for user      
-      return this.setState({      
-        latestTxId: parsedTx.txid,
-        answers: [parsedTx ,...this.state.answers],
-        questions: [parsedTx, ...this.state.questions]
-      })    
-    }    
-  }
-addressDetails()
-*/
