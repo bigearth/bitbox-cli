ed5bc5696f067c541cdb20bc91851c2ab3ed3b9d
Gabriel Cardona <gabriel@bitcoin.com>
2019-05-08 11:32:16 +0900

    Bump MAJOR version.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const repl = require("repl")
 const BITBOX = require("./lib/BITBOX").BITBOX
 const clone = require("git-clone")
 
-program.version("6.1.2 ", "-v, --version")
+program.version("7.0.0 ", "-v, --version")
 
 program
   .command("new <name>")
