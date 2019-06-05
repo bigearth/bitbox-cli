b03a81302059e587d5de1c300a8fb2542f95ec15
Chris Troutner <chris.troutner@gmail.com>
2019-06-05 15:16:05 -0700

    mocked out unit test for price class

--- test/unit/Price.ts
+++ test/unit/Price.ts
@@ -1,12 +1,19 @@
 // imports
-import * as assert from "assert";
+import * as chai from "chai"
 import { BITBOX } from "../../lib/BITBOX"
 import { Price } from "../../lib/Price"
+import axios from "axios"
+import * as sinon from "sinon"
 
 // consts
 const bitbox: BITBOX = new BITBOX()
+const assert: Chai.AssertStatic = chai.assert
 
 describe("#Price", (): void => {
+  let sandbox: any
+  beforeEach(() => (sandbox = sinon.sandbox.create()))
+  afterEach(() => sandbox.restore())
+
   describe("#PriceConstructor", (): void => {
     it("should create instance of Price", (): void => {
       const price: Price = new Price()
@@ -17,8 +24,15 @@ describe("#Price", (): void => {
   describe("#current", (): void => {
     describe("#single currency", (): void => {
       it("should get current price for single currency", async () => {
+        // Mock out data for unit test, to prevent live network call.
+        const data: any = 39229
+        const resolved: any = new Promise(r => r({ data: { price: data } }))
+        sandbox.stub(axios, "get").returns(resolved)
+
         const result = await bitbox.Price.current("usd")
-        assert.notEqual(0, result)
+        //console.log(`result: ${JSON.stringify(result, null, 2)}`)
+
+        assert.isNumber(result)
       })
     })
   })
