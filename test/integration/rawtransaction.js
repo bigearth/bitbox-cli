a54b6f2bd3f9f95a2b813deb64803a2154d2a575
Chris Troutner <chris.troutner@gmail.com>
2019-06-10 11:12:33 -0700

    added npm script to target staging server with integration tests

--- test/integration/rawtransaction.js
+++ test/integration/rawtransaction.js
@@ -13,6 +13,8 @@ let bitbox = new BITBOX()
 
 if (process.env.SERVER === "local")
   bitbox = new BITBOX({ restURL: "http://localhost:3000/v2/" })
+if (process.env.SERVER === "stage")
+  bitbox = new BITBOX({ restURL: "https://rest.btctest.net/v2/" })
 
 // Inspect utility used for debugging.
 const util = require("util")
