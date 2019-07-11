08163ff81962cb94ddb94e4a1beb3f401d62cc7c
Chris Troutner <chris.troutner@gmail.com>
2019-07-11 11:29:29 -0700

    Adding control integration test

---
+++ test/integration/control.js
@@ -0,0 +1,33 @@
+const chai = require("chai")
+const assert = chai.assert
+const BITBOX = require("../../lib/BITBOX").BITBOX
+let bitbox = new BITBOX()
+
+if (process.env.SERVER === "local")
+  bitbox = new BITBOX({ restURL: "http://localhost:3000/v2/" })
+if (process.env.SERVER === "stage")
+  bitbox = new BITBOX({ restURL: "https://rest.btctest.net/v2/" })
+
+describe("#control", () => {
+  describe("#getNetworkInfo", () => {
+    it("should GET getNetworkInfo data", async () => {
+      const result = await bitbox.Control.getNetworkInfo()
+      //console.log(`getNetworkInfo: ${JSON.stringify(result, null, 2)}`)
+
+      assert.hasAnyKeys(result, [
+        "version",
+        "subversion",
+        "protocolversion",
+        "localservices",
+        "localrelay",
+        "timeoffset",
+        "networkactive",
+        "connections",
+        "networks",
+        "relayfee",
+        "excessutxocharge",
+        "warnings"
+      ])
+    })
+  })
+})
